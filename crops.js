6968faa8ceb25b8e38fdfa16555cd2e955478b15
ARI DINAR <dinarari@gmail.com>
2019-01-16 13:05:03 +0100

    Create a Farm class

---
+++ crops.js
@@ -0,0 +1,36 @@
+class Crop {
+    constructor(acres) {
+        this.acres = acres
+    }
+    getYieldInEuros() {
+        return this.getYieldInKg() * this.price
+    }
+
+    getCosts() {
+        return this.acres * this.cost
+    }
+}
+
+class Wheat extends Crop {
+    constructor(acres){
+        super(acres)
+        this.price = 1.5
+        this.cost = 340
+    }
+    getYieldInKg() {
+        return Math.pow(this.acres * 1.5, 1.3)
+    }
+}
+
+class Sugarcane extends Crop{
+    constructor(acres){
+        super(acres)
+        this.price = 2.0
+        this.cost = 166
+    }
+    getYieldInKg() {
+        return Math.pow(this.acres * 2.6,  1.1)
+    }
+}
+
+module.exports = { Crop, Wheat, Sugarcane }
---
+++ index.spec.js
@@ -0,0 +1,31 @@
+const { Farm } = require('./index')
+const { Wheat } = require('./crops')
+const { Sugarcane} = require('./crops')
+
+test('A new farm can be created', () => {
+    const farm = new Farm()
+    expect(farm).toBeTruthy()
+})
+
+test('A farm can have a name', () => {
+    const farm = new Farm('TEST_NAME')
+    expect(farm.name).toBe('TEST_NAME')
+})
+
+test('We can add a crop to our farm', () => {
+    const farm = new Farm('TEST_NAME')
+    farm.addCrop(new Wheat(100))
+  })
+
+  test('The income of an empty farm is 0', () => {
+    const farm = new Farm('TEST_NAME')
+    expect(farm.calculateIncome()).toBe(0)
+  })
+
+  test('The income of a 100 Wheat + 100 Sugarcane farm is X', () => {
+    const farm = new Farm('TEST_NAME')
+    farm.addCrop(new Wheat(100))
+    farm.addCrop(new Sugarcane(100))
+    expect(farm.calculateIncome()).toBeCloseTo(1918.37, 1)
+  })
+
